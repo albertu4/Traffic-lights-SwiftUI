702e8cc1e7465d2c3648f5c5d4c4deb2071c3422
Miwka <97275239+albertu4@users.noreply.github.com>
2022-05-15 21:05:10 +0900

    Initial Commit

---
+++ Traffic lights/Traffic_lightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_lightsApp.swift
+//  Traffic lights
+//
+//  Created by Михаил Иванов on 15.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_lightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
